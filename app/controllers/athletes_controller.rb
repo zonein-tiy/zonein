1c521058760df7a99f83b3402eee0b2cd7f3a443
Robert Smiles Jr <smilesr120@gmail.com>
2015-07-07 14:48:34 -0400

    re-added render to register

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -1,6 +1,6 @@
 class AthletesController < ApplicationController
 # skip_before_filter  :verify_authenticity_token
-before_action :authenticate_with_token!, only: [:register]
+before_action :authenticate_with_token!, only: [:register, :signin]
 
   def signup
     @athlete = Athlete.new(username: params[:username],
@@ -17,7 +17,7 @@ before_action :authenticate_with_token!, only: [:register]
   def register
     @athlete = current_athlete
     @athlete.update(first_name: params[:first_name], last_name: params[:last_name], phone_number: params[:phone_number], home_address: params[:home_address], city: params[:city], state: params[:state], zip: params[:zip], interests: params[:interests], image_url: params[:image_url])
-
+    render 'register.json.jbuilder'
    # if @athlete.update
    #    render 'register.json.jbuilder'
    # else
