da43d59cfa9128283459382f662a5a0c8cc42ae5
Robert Smiles Jr <smilesr120@gmail.com>
2015-07-09 12:44:07 -0400

    updated schema in light of changes to tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709153655) do
+ActiveRecord::Schema.define(version: 20150709161527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20150709153655) do
     t.integer  "workout_interval"
     t.boolean  "completion"
     t.string   "created_by"
+    t.integer  "creator_id"
   end
 
   create_table "teams", force: :cascade do |t|
@@ -72,6 +73,7 @@ ActiveRecord::Schema.define(version: 20150709153655) do
     t.datetime "season_end"
     t.string   "city"
     t.string   "state"
+    t.integer  "coach_id"
   end
 
   create_table "workouts", force: :cascade do |t|
@@ -83,6 +85,7 @@ ActiveRecord::Schema.define(version: 20150709153655) do
     t.boolean "running",       default: false
     t.boolean "weightlifting", default: false
     t.text    "steps"
+    t.integer "creator_id"
   end
 
 end
